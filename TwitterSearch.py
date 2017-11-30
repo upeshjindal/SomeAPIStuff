03f024eb2109ede5a2cd4f7dc0ea885618bfe3c3
pedalong85 <pedalong85@gmail.com>
2017-11-30 16:28:14 -0600

    Fix the code to read the tokens correctly. Also, add functionality to call TwitterAPI.

--- TwitterSearch.py
+++ TwitterSearch.py
@@ -1,10 +1,17 @@
+from TwitterAPI import TwitterAPI
 
+# Initialize some variables
 consumerkey     = ""
 consumersecret  = ""
 accesstoken     = ""
 accesssecret    = ""
 
 for linetoken in open("tokens.txt"):
+
+    # Remove the newline
+    linetoken = linetoken[0:len(linetoken)-1]
+
+    # Check the specific tokens and assign them
     if ("consumerkey" in linetoken):
         consumerkey = linetoken.split(":")[1]
     if ("consumersecret" in linetoken):
@@ -14,8 +21,18 @@ for linetoken in open("tokens.txt"):
     if ("accesssecret" in linetoken):
         accesssecret = linetoken.split(":")[1]
 
-print("consumerkey: " + consumerkey)
+# Some debugging prints
+
+print("consumerkey: " + consumerkey + ":")
 print("consumersecret: " + consumersecret)
 print("accesstoken: " + accesstoken)
 print("accesssecret: " + accesssecret)
 
+# Call the TwitterAPI construct
+api = TwitterAPI(consumerkey, consumersecret, accesstoken, accesssecret)
+
+# Search some tweets
+tweets = api.request('search/tweets', {'q': 'h1b', 'count': 100})
+
+# Print them
+print (tweets.text)
